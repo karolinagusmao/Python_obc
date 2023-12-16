f13eec857dbee42832262dfb28500e8284a6d6fe
karolinagusmao <karolinab2gusmao@gmail.com>
2023-12-15 22:31:17 -0300

    second lesson

---
+++ 2-types.py
@@ -0,0 +1,15 @@
+name = "Call of Duty - Modern Warfare III"
+yearLaunch = 2023
+gamePrice = 399.00
+planIncluded = False
+
+print(name)
+print(yearLaunch)
+print(gamePrice)
+print(planIncluded)
+
+print(type(name))
+print(type(yearLaunch))
+print(type(gamePrice))
+print(type(planIncluded))
+
