77243e09bfb19d5419964d5aee5c5da273f04790
karolinagusmao <karolinab2gusmao@gmail.com>
2023-12-15 22:42:26 -0300

    lesson 3 - input

---
+++ 3-input.py
@@ -0,0 +1,14 @@
+# Utilizando o input '''multi comentarios
+'''
+Jogoteca
+'''
+
+name = input("Digite o nome do jogo\n")
+yearLaunch = int(input("Digite o ano de lançamento do jogo\n")) #\n quebra a linha
+gamePrice = float(input("Digite o preço do jogo\n"))
+planIncluded = bool(input("Está incluso no serço mensal?\n"))
+
+print (name)
+print (planIncluded)
+print (gamePrice)
+print (planIncluded)
