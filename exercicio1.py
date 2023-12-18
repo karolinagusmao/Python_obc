188481c6defc26ef4eddfa80eca0fd2a3d435d2f
karolinagusmao <karolinab2gusmao@gmail.com>
2023-12-18 12:16:25 -0300

    "desafio"

---
+++ exercicio1.py
@@ -0,0 +1,15 @@
+#Antecessor e sucessor de um numero
+
+number = int(input("Digite um número\n"))
+print(f"Antecessor do número: {number -1} | Sucessor do número: {number +1}")
+
+#Media de 4 notas
+
+note1 = float(input("Digite a nota 1\n"))
+note2 = float(input("Digite a nota 2\n"))
+note3 = float(input("Digite a nota 3\n"))
+note4 = float(input("Digite a nota 4\n"))
+
+average = (note1 + note2 + note3 + note4) /4
+
+print(f"Médias das notas é: {average:.2f}")
